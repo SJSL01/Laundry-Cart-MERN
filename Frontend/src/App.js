b8f4146ea556bcc1742ca1ffed9e7e1bdcad3066
SJSL01 <simran08lall@gmail.com>
2022-12-19 03:07:22 +0530

    Frontend Template

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Components/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <BrowserRouter>
+
+      </BrowserRouter>
+    </Layout>
+  );
+}
+
+export default App;
