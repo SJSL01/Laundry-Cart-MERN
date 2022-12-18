ccc8af76079212e038287c15b8e474586c71e200
SJSL01 <simran08lall@gmail.com>
2022-12-19 02:53:36 +0530

    Backend Template

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+// Imports
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const connect_database = require("./Connect Database/")
+
+// App Configs
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan("common"))
+
+
+// Routes
+
+
+
+
+////
+
+
+// Connect to database and server listining
+app.listen(process.env.PORT, async () => {
+    await connect_database()
+    console.log(`Server up at port ${process.env.PORT}`);
+})
