2eeb382d093637b29f563605fb5f7d9b4bcb31e5
Arockiajeyson <aarockiajeyson007@gmail.com>
2022-12-21 23:53:38 +0530

    done tracking comp

--- Frontend/src/Components/Tracking.jsx
+++ Frontend/src/Components/Tracking.jsx
@@ -3,6 +3,7 @@ import '../Styles/tracking.css'
 export default function Tracking() {
   return (
     <div className='main-div-tracking'>
+      
         <div className='tracking'></div><p className='para-div'>Picked up</p><hr className='underline-1' style={{width:'150px'}}/>
         <div className='tracking-div'></div><p className='para-div'>Washed</p><hr className='underline-1' style={{width:'150px'}}/>
         <div className='tracking-div'></div><p className='para-div'>Ironed</p><hr className='underline-1' style={{width:'150px'}}/>
